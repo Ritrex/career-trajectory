4467a4bb0459207ae422480d3b638b80da4f0ae4
nancy27vazquez <nevadg@outlook.com>
2019-10-26 05:23:28 -0500

    Last version of changes to views

--- routes/newAuction.js
+++ routes/newAuction.js
@@ -1,10 +1,10 @@
 const express = require("express");
 const router = express.Router();
 const updloader = require("../helpers/upload");
-const Sale=require("../models/Sale")
+const Sale = require("../models/Sale");
 /* GET home page */
 router.get("/newAuction/new", (req, res, next) => {
-  res.render("newAuction", { register: true });
+  res.render("newAuction", { register: true, user: req.user });
 });
 
 router.post(
@@ -12,10 +12,14 @@ router.post(
   updloader.fields([
     { name: "ticket_full", maxCount: 1 },
     { name: "ticket_partial", maxCount: 1 },
-    { name: "ticket_detail",maxCount:1}
+    { name: "ticket_detail", maxCount: 1 }
   ]),
   (req, res) => {
-    console.log("Did you get what I sent you?:",req.files['ticket_partial'],req.files['ticket_detail']);
+    console.log(
+      "Did you get what I sent you?:",
+      req.files["ticket_partial"],
+      req.files["ticket_detail"]
+    );
     //req.files.map(file=>file.secure_url)
     //  req.files.map()
     //console.log(req.file,req.files,res.body);
@@ -28,28 +32,35 @@ router.post(
       time_two,
       place_three,
       time_three
-    } = req.body
+    } = req.body;
 
-    let item={
+    let item = {
       event_date,
-      src_url_public:req.files['ticket_partial'][0].secure_url,
-      src_url_private:req.files['ticket_full'][0].secure_url,
-      prefered_locations:[
-        {lugar:place_one,horario:time_one},
-        {lugar:place_two,horario:time_two},
-        {lugar:place_three,horario:time_three}
+      src_url_public: req.files["ticket_partial"][0].secure_url,
+      src_url_private: req.files["ticket_full"][0].secure_url,
+      prefered_locations: [
+        { lugar: place_one, horario: time_one },
+        { lugar: place_two, horario: time_two },
+        { lugar: place_three, horario: time_three }
       ]
-    }
-    let sale={item,event_arena, min_price, max_price,name,userid:req.user._id}
+    };
+    let sale = {
+      item,
+      event_arena,
+      min_price,
+      max_price,
+      name,
+      userid: req.user._id
+    };
     Sale.create(sale)
-    .then(succ=>{
-      console.log("venta arriba")
-      res.redirect("/")
-    })
-    .catch(error=>{
-      console.log(error)
-      res.redirect("/newAuction/new")
-    })
+      .then(succ => {
+        console.log("venta arriba");
+        res.redirect("/");
+      })
+      .catch(error => {
+        console.log(error);
+        res.redirect("/newAuction/new");
+      });
   }
 );
 module.exports = router;
